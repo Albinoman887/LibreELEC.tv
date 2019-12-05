fead28d1777bf1f0d839e89667ab8641db148bf3
Matt Filetto <matt.filetto@gmail.com>
2019-12-05 03:06:49 -0800

    Revert "nov11-globals branch of kodi projectm addon (probably isnt needed) saving"
    * not needed. all isssues were in actual libprojectm rc7 fixes the issue
    
    This reverts commit 628b1e8ad32d8e8ba62715bf45c7f207bfa02622.

--- packages/mediacenter/kodi-binary-addons/visualization.projectm/package.mk
+++ packages/mediacenter/kodi-binary-addons/visualization.projectm/package.mk
@@ -3,13 +3,13 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="visualization.projectm"
-PKG_VERSION="nov11-globals"
-PKG_SHA256=""
-PKG_REV="nov11-globals"
+PKG_VERSION="2.3.2-Leia"
+PKG_SHA256="e30163b4eb12518deb1b56c488b0a81f7eeb3467d291dad6e28234843fc9dc40"
+PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/Albinoman887/visualization.projectm"
-PKG_URL="https://github.com/Albinoman887/visualization.projectm/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/xbmc/visualization.projectm"
+PKG_URL="https://github.com/xbmc/visualization.projectm/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain kodi-platform libprojectM"
 PKG_SECTION=""
 PKG_SHORTDESC="visualization.projectm"
